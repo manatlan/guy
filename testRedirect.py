058a13a265d83232b65008c4fa7d367165e8ef21
manatlan <manatlan@gmail.com>
2019-09-17 20:20:12 +0200

    add test/py

---
+++ testRedirect.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from guy import Guy
+
+class Glob:
+  i=0
+  def test(self):
+    Glob.i+=1
+    return Glob.i
+
+#==========================================
+class Marco(Guy,Glob):
+#==========================================
+  """ Hello Marco
+
+      <script>
+      async function aff( am ) {document.body.innerHTML+=await am();}
+      </script>
+
+      <button onclick="aff( self.t1 )">t1</button>
+      <button onclick="aff( self.s1 )">s1</button>
+      <button onclick="aff( self.test )">Test</button>
+
+
+      <a href="/Polo">go to polo</a>
+      <a href="/nowhere">go to nowhere</a>
+      <a href="/logo.png">go to logo.png</a>
+
+      <button onclick="self.open()">open</button>
+  """
+
+  def open(self):
+    return Polo()
+
+  def t1(self):
+    return "t1"
+
+  cpt=0
+  def s1(self):
+    self.cpt+=1
+    return self.cpt
+
+#==========================================
+class Polo(Guy,Glob):
+#==========================================
+  """ Hello Polo
+
+      <script>
+      async function aff( am ) {document.body.innerHTML+=await am();}
+      </script>
+
+      <button onclick="aff( self.t2 )">t2</button>
+      <button onclick="aff( self.s2 )">s2</button>
+      <button onclick="aff( self.test )">Test</button>
+
+      <a href="/">go to marco</a>
+  """
+
+  def t2(self):
+    return "t2"
+
+  cpt=0
+  def s2(self):
+    self.cpt+=1
+    return self.cpt
+
+if __name__ == "__main__":
+    app=Marco()
+    app.serve(log=True)
+    #~ app.run()
+
