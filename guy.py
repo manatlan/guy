144f45606d380bf4cbd5d6f90908632d9c105ecd
manatlan <manatlan@gmail.com>
2019-09-17 20:27:14 +0200

    clean repo

--- guy.py
+++ guy.py
@@ -1094,40 +1094,5 @@ def runAndroid(ga):
 if __name__ == "__main__":
 
     #~ from testTordu import Tordu as GuyApp
-    # from testPrompt import Win as GuyApp
-    # GuyApp().run()
-    #~ GuyApp().server(8000)
-
-    class T(Guy):
-        __doc__="""
-        <script>
-        var word="";
-
-        guy.on( "hello", async function(letter) {
-            word+=letter;
-        })
-
-        guy.on( "end", async function() {
-            await self.endtest(word)
-        })
-
-        guy.init( async function() {
-            guy.emitMe("hello","B")     // avoid socket, but it counts
-            guy.emit("hello","C")       // emit all clients
-            await self.makeEmits()       // generate server emits
-        })
-
-        guy.emitMe("hello","A")         // avoid socket, so can be run before init
-
-        </script>
-        """
-        async def makeEmits(self):
-            await self.emit("hello","D")      # emit all clients
-            await self.emitMe("hello","E")         # emit ME only
-            await self.emitMe("end")               # emit ME only and finnish the test
-        def endtest(self,word):
-            self.word=word
-            self.exit()
-    t=T()
-    r=t.runCef()
-    assert r.word=="ABCDE"
+    from testPrompt import Win as GuyApp
+    GuyApp().run()
