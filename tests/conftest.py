6dabfe36edf4c4fabddb529d0dd876a28598fb42
manatlan <manatlan@gmail.com>
2019-09-17 20:07:58 +0200

    add test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,13 @@
 import pytest
 
-# @pytest.fixture(params=["run","runCef","server"])
-@pytest.fixture(params=["run","runCef"])
+@pytest.fixture(params=["run","runCef","serve"])
+# @pytest.fixture(params=["run","runCef"])
 # @pytest.fixture(params=["run"])
-# @pytest.fixture(params=["server"])
+# @pytest.fixture(params=["serve"])
 # @pytest.fixture(params=["runCef"])
 def runner(request):
     def _( ga ):
-        if request.param=="server":
+        if request.param=="serve":
             return getattr(ga,request.param)(port=10000)
         else:
             return getattr(ga,request.param)()
---
+++ tests/test_600_redirect.py
@@ -0,0 +1,40 @@
+
+from guy import Guy
+
+def test_redirect(runner):
+    class W1(Guy):
+        __doc__="""
+        <script>
+        guy.init( async function() {
+            if(document.location.href.indexOf("#W1")>=0) { // first call
+                if(await self.step1())
+                    document.location.href="/W2"
+            }
+            else
+                self.step3()
+        })
+        </script>
+        1
+        """
+        def step1(self):
+            return True
+        def step3(self):
+            self.ok=True
+            self.exit()
+
+    class W2(Guy):
+        __doc__="""
+        <script>
+        guy.init( async function() {
+            if(await self.step2())
+                document.location.href="/W1"
+        })
+        </script>
+        2
+        """
+        def step2(self):
+            return True
+
+    t=W1()
+    r=runner(t)
+    assert r.ok
